f3aefe32a35f6b0b213320fffb5166f61fbe9234
William Girouard-Miot <6245166@collegemv.qc.ca>
2024-02-08 15:48:59 -0500

    Ajout des taches

--- fonction.js
+++ fonction.js
@@ -14,7 +14,33 @@ function saveSession() {
 
 function restaurer() {
     const nom = localStorage.getItem('nom');
-    console.log('Nom: ', nom)
+    console.log('Nom: ', nom);
     const password = localStorage.getItem('password');
-    console.log('Password: ', password)
+    console.log('Password: ', password);
+}
+const taskInput = document.getElementById('taksInput');
+const addTaskBtn = document.getElementById('addTaskBtn');
+const taskList = document.getElementById('taskList');
+
+addTaskBtn.addEventListener("click", addTask);
+function ajoutTache() {
+    const taskText = taskInput.value.trim();
+    if (taskText !== "") {
+        const listItem = document.createElement('li');
+        listItem.textContent = taskText;
+        taskList.appendChild(listItem)
+        taskInput.value = "";
+        //Ajout d'un bouton de suppression
+        const deleteBtn = document.createElement("button");
+        deleteBtn.textContent = "Supprimer"
+        
+        listItem.appendChild(deleteBtn);
+
+        deleteBtn.addEventListener("click", () => {
+            listItem.remove();
+        });
+    } else {
+        alert("Veuillez entrer une tâche valide.")
+    }
+ 
 }
