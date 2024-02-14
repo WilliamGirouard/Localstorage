558b95d2f029ebb45de362214060ffe995df67e8
William Girouard-Miot <6245166@collegemv.qc.ca>
2024-02-14 14:29:07 -0500

    Modification

--- fonction.js
+++ fonction.js
@@ -24,15 +24,11 @@ const taskList = document.getElementById("taskList");
 var task = "task";
 var i = 0;
 function w3_open() {
-document.getElementById("main").style.marginLeft = "25%";
-document.getElementById("mySidebar").style.width = "25%";
-document.getElementById("mySidebar").style.display = "block";
-document.getElementById("openNav").style.display = 'none';
+  document.getElementById("mySidebar").style.display = "block";
 }
+
 function w3_close() {
-document.getElementById("main").style.marginLeft = "0%";
-document.getElementById("mySidebar").style.display = "none";
-document.getElementById("openNav").style.display = "inline-block";
+  document.getElementById("mySidebar").style.display = "none";
 }
 
 addTaskBtn.addEventListener("click", addTask);
